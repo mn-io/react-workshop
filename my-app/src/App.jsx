bf7d3a4c5e91ee733d19bf7c74f3c8b7debb4891
Mario Neises <git@mnio.net>
2018-11-19 10:19:23 +0100

    input form with keyhandler

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,25 @@ import './App.css';
 import HeaderComponent from './HeaderComponent';
 
 class App extends React.Component {
+
+  handleOnKeyUp = (event) => {
+    const val = event.target.value
+    console.log({ val })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <HeaderComponent name='World'></HeaderComponent>
+          <input type='text'
+            style={{ height: '1rem', marginTop: '1rem' }}
+            onKeyUp={
+              this.handleOnKeyUp
+            }>
+          </input>
         </header>
-      </div>
+      </div >
     );
   }
 }
